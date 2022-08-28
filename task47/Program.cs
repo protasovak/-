8bf472bb5479031ffddb2d5feaebe1f036f01737
«Roman» <kseniya-prot@yandex.ru>
2022-08-28 20:11:11 +0300

    task47

---
+++ task47/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+Console.Write("Введите количество строк: ");
+int line = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+double[,] a = new double[line, column];
+
+Random random = new Random();
+for (int i = 0; i < line; i++)
+{
+    for (int j = 0; j < column; j++)
+    {
+        a[i, j] = random.NextDouble() * 100;
+        Console.Write("{0,6:F1}", a[i, j]);
+    }
+    Console.WriteLine();
+}
